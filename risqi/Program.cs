7b3d1a6ec882e1b193ac468f5eb020759108c8c8
RIZQIFAKHRIANTO <risqifakhrianto@gmail.com>
2024-08-16 17:52:07 +0700

    Add files via upload

---
+++ risqi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiListBilanganPrima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikasi List Bilangan Prima");
+            Console.WriteLine("============================");
+            Console.WriteLine("Nama : Rizqi Fakhrianto");
+            Console.WriteLine("Kelas : X PPLG");
+            Console.WriteLine("Absen : 30");
+
+            for (int num = 2; num <= 1000; num++)
+            {
+                bool isPrima = true;
+                for (int pembagi = 2; pembagi <= Math.Sqrt(num); pembagi++)
+                {
+                    if (num % pembagi == 0)
+                    {
+                        isPrima = false;
+                        break;
+                    }
+                }
+                if (isPrima)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
